4b693c7c5ba523878e4749ccd3e7ee78e5db5f86
Andrea Celentano <andrea.celentano@ge.infn.it>
2018-04-18 18:59:41 +0200

    v2

---
+++ BDXDSTSelector2.cc
@@ -0,0 +1,216 @@
+#define BDXDSTSelector2_cxx
+// The class definition in BDXDSTSelector.h has been generated automatically
+// by the ROOT utility TTree::MakeSelector(). This class is derived
+// from the ROOT class TSelector. For more information on the TSelector
+// framework see $ROOTSYS/README/README.SELECTOR or the ROOT User Manual.
+
+// The following methods are defined in this file:
+//    Begin():        called every time a loop on the tree starts,
+//                    a convenient place to create your histograms.
+//    SlaveBegin():   called after Begin(), when on PROOF called only on the
+//                    slave servers.
+//    Process():      called for each event, in this function you decide what
+//                    to read and fill your histograms.
+//    SlaveTerminate: called at the end of the loop on the tree, when on PROOF
+//                    called only on the slave servers.
+//    Terminate():    called at the end of the loop on the tree,
+//                    a convenient place to draw/fit your histograms.
+//
+// To use this file, try the following session on your Tree T:
+//
+// root> T->Process("BDXDSTSelector.C")
+// root> T->Process("BDXDSTSelector.C","some options")
+// root> T->Process("BDXDSTSelector.C+")
+//
+
+#include "BDXDSTSelector2.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TRandom3.h>
+#include <TApplication.h>
+#include <TGraph.h>
+#include <TCanvas.h>
+#include <vector>
+
+void BDXDSTSelector2::Begin(TTree * /*tree*/) {
+	// The Begin() function is called at the start of the query.
+	// When running with PROOF Begin() is only called on the client.
+	// The tree argument is deprecated (on PROOF 0 is passed).
+
+	TString option = GetOption();
+
+	thisEventFineTime = 0;
+
+}
+
+void BDXDSTSelector2::SlaveBegin(TTree * /*tree*/) {
+	// The SlaveBegin() function is called after the Begin() function.
+	// When running with PROOF SlaveBegin() is called on each slave server.
+	// The tree argument is deprecated (on PROOF 0 is passed).
+	TString option = GetOption();
+	Info("SlaveBegin", "start with options: %s", option.Data());
+
+	int N = (int) (this->Ttot / this->Tbin) + 1;
+
+	/*Create here the histograms.*/
+	hTrigAllEventsBeam = new TH1D("hTrigAllEventsBeam", "hTrigAllEventsBeam", N, 0, N * Tbin);
+	hTrigAllEventsCosmics = new TH1D("hTrigAllEventsCosmics", "hTrigAllEventsCosmics", N, 0, N * Tbin);
+
+	hEneCrystalBeam = new TH1D("hEneCrystalBeam", "hEneCrystalBeam", 200, 0, 2000);
+	hEneCrystalCosmics = new TH1D("hEneCrystalCosmics", "hEneCrystalCosmics", 200, 0, 2000);
+
+	Info("SlaveBegin", "AllHistos to fOutput");
+	TIter next(gDirectory->GetList());
+	TObject *obj;
+	while (obj = (TObject*) next()) {
+		if (obj->InheritsFrom(TH1::Class())) {
+			fOutput->Add(obj);
+		}
+	}
+
+	Info("SlaveBegin", "end");
+}
+
+Bool_t BDXDSTSelector2::Process(Long64_t entry) {
+	// The Process() function is called for each entry in the tree (or possibly
+	// keyed object in the case of PROOF) to be processed. The entry argument
+	// specifies which entry in the currently loaded tree is to be processed.
+	// When processing keyed objects with PROOF, the object is already loaded
+	// and is available via the fObject pointer.
+	//
+	// This function should contain the \"body\" of the analysis. It can contain
+	// simple or elaborate selection criteria, run algorithms on the data
+	// of the event and typically fill histograms.
+	//
+	// The processing can be stopped by calling Abort().
+	//
+	// Use fStatus to set the return value of TTree::Process().
+	//
+	// The return value is currently not used.
+	this->GetEntry(entry);
+
+	/*Objects to read collections*/
+	CalorimeterHit *fCaloHit;
+	IntVetoHit *fIntVetoHit;
+
+	/*Variables*/
+	int tWord;
+	int ibin;
+	bool isCrystal = false;
+
+	int eventType;
+
+	double eneCorr;
+
+	/*Get the event header and fill some variables*/
+	m_EventHeader = m_Event->getEventHeader();
+	m_epicsData = m_EventHeader->getEpicsData();
+	eventNumber = m_EventHeader->getEventNumber();
+	runNumber = m_EventHeader->getRunNumber();
+
+	if (m_EventHeader == 0) return kTRUE;
+	if (m_EventHeader->getTriggerWords().size() == 0) return kTRUE;
+	thisEventT = m_Event->getEventHeader()->getEventTime() - T0;
+
+	/*Check the type of this event: beam, cosmics, none*/
+	ibin = hTimeIntervals->FindBin(thisEventT);
+	eventType = hTimeIntervals->GetBinContent(ibin);
+	if (eventType == 0) return kTRUE;
+
+	/*Check the energy correction*/
+	ibin = hEnergyCorrection->FindBin(thisEventT);
+	eneCorr = hEnergyCorrection->GetBinContent(ibin);
+	if (eneCorr <= 0) return kTRUE;
+
+	if (m_epicsData->hasData("hac_bcm_average")) {
+		//thisEventT= m_epicsData->getDataTime("hac_bcm_average")-T0;
+		//	cout<<"1: "<<m_Event->getEventHeader()->getEventTime()<<" "<<m_epicsData->getDataTime("hac_bcm_average")<<" "<<m_epicsData->getDataValue("hac_bcm_average")<<endl;
+
+	}
+
+	if (m_epicsData->hasData("HALLA:p")) {
+		//thisEventT= m_epicsData->getDataTime("hac_bcm_average")-T0;
+		//cout<<"2: "<<m_Event->getEventHeader()->getEventTime()<<" "<<m_epicsData->getDataTime("HALLA:p")<<" "<<m_epicsData->getDataValue("HALLA:p")<<endl;
+
+	}
+
+	if (m_epicsData->hasData("B_DET_BDX_FPGA:livetime")) {
+
+		//thisEventT= m_epicsData->getDataTime("B_DET_BDX_FPGA:livetime")-T0;
+
+	}
+
+	tWord = m_EventHeader->getTriggerWords()[0];
+	if ((tWord >> 4) & 0x1) isCrystal = true;
+
+	/*BEAM*/
+	switch (eventType) {
+	case 1:
+		hTrigAllEventsBeam->Fill(thisEventT);
+		break;
+	case 2:
+		hTrigAllEventsCosmics->Fill(thisEventT);
+		break;
+
+	}
+
+	if (isCrystal == false) return kTRUE;
+
+	if (m_Event->hasCollection(CalorimeterHit::Class(), "CalorimeterHits")) {
+		TIter CaloHitsIter(m_Event->getCollection(CalorimeterHit::Class(), "CalorimeterHits"));
+		while (fCaloHit = (CalorimeterHit*) CaloHitsIter.Next()) { //Need to cast to the proper object
+			if ((fCaloHit->m_channel.sector == 0) && (fCaloHit->m_channel.x == 1) && (fCaloHit->m_channel.y == 0)) {
+				switch (eventType) {
+				case 1:
+					hEneCrystalBeam->Fill(fCaloHit->E * eneCorr);
+					break;
+				case 2:
+					hEneCrystalCosmics->Fill(fCaloHit->E * eneCorr);
+					break;
+				}
+			}
+		}
+	}
+
+	return kTRUE;
+}
+
+void BDXDSTSelector2::SlaveTerminate() {
+// The SlaveTerminate() function is called after all entries or objects
+// have been processed. When running with PROOF SlaveTerminate() is called
+// on each slave server.
+
+}
+
+void BDXDSTSelector2::Terminate() {
+// The Terminate() function is the last function to be called during
+// a query. It always runs on the client, it can be used to present
+// the results graphically or save the results to file.
+
+	Info("Terminate", "Terminate starts");
+	Info("Terminate", "Total events are: %i", nEventsTotal);
+
+	TListIter iter(fOutput);
+	TObject *obj;
+	int binx1, binx2;
+	double norm, thr;
+
+	while (obj = iter.Next()) {
+		Info("Terminate", "obj: %s", obj->GetName());
+	}
+	Info("Terminate", "No more objs");
+
+	hTrigAllEventsBeam = (TH1D*) fOutput->FindObject("hTrigAllEventsBeam");
+	hTrigAllEventsCosmics = (TH1D*) fOutput->FindObject("hTrigAllEventsCosmics");
+
+	hEneCrystalBeam = (TH1D*) fOutput->FindObject("hEneCrystalBeam");
+	hEneCrystalCosmics = (TH1D*) fOutput->FindObject("hEneCrystalCosmics");
+
+	hTrigAllEventsBeam->Sumw2();
+	hTrigAllEventsBeam->Scale(1., "width");
+
+	hTrigAllEventsCosmics->Sumw2();
+	hTrigAllEventsCosmics->Scale(1., "width");
+
+}
+
---
+++ BDXDSTSelector2.h
@@ -0,0 +1,205 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Tue Dec 27 16:37:57 2016 by ROOT version 6.08/00
+// from TTree EventDST/EventDST
+// found on file: outTest.root
+//////////////////////////////////////////////////////////
+
+#ifndef BDXDSTSelector2_h
+#define BDXDSTSelector2_h
+
+#include <TROOT.h>
+#include "TProofOutputFile.h"
+#include <TChain.h>
+#include <TFile.h>
+#include <TSelector.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
+#include <TClonesArray.h>
+#include <TCollection.h>
+
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TAxis.h>
+#include <TCanvas.h>
+
+// Headers needed by this particular selector
+#include "EventBuilder/TEvent.h"
+#include "EventBuilder/TEventHeader.h"
+
+#include "IntVeto/IntVetoDigiHit.h"
+#include "Calorimeter/CalorimeterDigiHit.h"
+
+#include "Calorimeter/CalorimeterHit.h"
+#include "IntVeto/IntVetoHit.h"
+#include "ExtVeto/ExtVetoHit.h"
+
+#include "Calorimeter/CalorimeterMCRealHit.h"
+
+#include "EventBuilder/TEventHeader.h"
+#include "EPICS/epicsData.h"
+
+class BDXDSTSelector2: public TSelector {
+public:
+
+	TTree *fChain = 0;   //!pointer to the analyzed TTree or TChain
+
+	// Readers to access the data
+	//TTreeReaderValue<TEvent> event = { fReader, "Event" };
+
+	BDXDSTSelector2(TTree * /*tree*/= 0) {
+		/*All the histogram pointers MUST be initialized to 0 here*/
+		m_EventHeader = 0;
+		m_epicsData = 0;
+		m_Event = 0;
+
+
+		Ttot = 0;
+		T0 = 0;
+
+		nEventsTotal = 0;
+		thisEventN = 0;
+		thisEventT = 0;
+
+		hTrigAllEventsBeam=0;
+		hTrigAllEventsCosmics=0;
+		hTimeIntervals=0;
+		hEnergyCorrection=0;
+
+		hEneCrystalBeam=0;
+		hEneCrystalCosmics=0;
+
+
+		NProof=0;
+		runNumber=0;
+		eventNumber=0;
+
+		Tbin=300;
+
+	}
+	virtual ~BDXDSTSelector2() {
+	}
+	virtual Int_t Version() const {
+		return 2;
+	}
+	virtual void Begin(TTree *tree);
+	virtual void SlaveBegin(TTree *tree);
+	virtual void Init(TTree *tree);
+	virtual Bool_t Notify();
+	virtual Bool_t Process(Long64_t entry);
+	virtual Int_t GetEntry(Long64_t entry, Int_t getall = 0) {
+		return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0;
+	}
+	virtual void SetOption(const char *option) {
+		fOption = option;
+	}
+	virtual void SetObject(TObject *obj) {
+		fObject = obj;
+	}
+	virtual void SetInputList(TList *input) {
+		fInput = input;
+	}
+	virtual TList *GetOutputList() const {
+		return fOutput;
+	}
+	virtual void SlaveTerminate();
+	virtual void Terminate();
+
+
+	int eventNumber, runNumber;
+
+
+	/*Pointers. Note that EACH histogram pointer MUST be initialized to 0 in the TSelector constructor*/
+	TEventHeader *m_EventHeader;
+	TEvent *m_Event;
+	epicsData *m_epicsData;
+
+
+	TH1D *hTimeIntervals;
+	TH1D *hEnergyCorrection;
+
+	TH1D *hTrigAllEventsBeam;
+	TH1D *hTrigAllEventsCosmics;
+
+	TH1D *hEneCrystalBeam;
+	TH1D *hEneCrystalCosmics;
+
+
+	/*Variables*/
+	double Ttot, T0,Tbin;
+	int thisEventN;
+	double thisEventT;
+	int nEventsTotal;
+	int NProof;
+
+	uint64_t thisEventFineTime;
+
+
+	/*Methods*/
+	double getTimeInterval() {
+		return Ttot;
+	}
+	void setTimeInterval(double T) {
+		Ttot = T;
+		Info("setTimeInterval", Form("Ttot set to %f", Ttot));
+	}
+	void setTimeBin(double T){
+		Tbin = T;
+		Info("setTimeBin","Time bin set to %f",Tbin);
+	}
+	double getT0() {
+		return T0;
+	}
+	void setT0(double T) {
+		T0 = T;
+		Info("setT0", Form("time0 set to %f", T0));
+	}
+
+	void sethTimeIntervals(TH1D *h){
+		hTimeIntervals=h;
+	}
+
+	void sethEnergyCorrection(TH1D *h){
+			hEnergyCorrection=h;
+		}
+
+	void setNProof(int N) {
+		NProof = N;
+	}
+
+
+ClassDef(BDXDSTSelector2,1);
+
+};
+
+#endif
+
+#ifdef BDXDSTSelector2_cxx
+void BDXDSTSelector2::Init(TTree *tree)
+{
+	// The Init() function is called when the selector needs to initialize
+	// a new tree or chain. Typically here the reader is initialized.
+	// It is normally not necessary to make changes to the generated
+	// code, but the routine can be extended by the user if needed.
+	// Init() will be called many times when running on PROOF
+	// (once per file to be processed).
+	if (!tree) return;
+	fChain = tree;
+
+	fChain->SetBranchAddress("Event", &m_Event);
+
+}
+
+Bool_t BDXDSTSelector2::Notify()
+{
+	// The Notify() function is called when a new file is opened. This
+	// can be either for a new TTree in a TChain or when when a new TTree
+	// is started when using PROOF. It is normally not necessary to make changes
+	// to the generated code, but the routine can be extended by the
+	// user if needed. The return value is currently not used.
+
+	return kTRUE;
+}
+
+#endif // #ifdef BDXDSTSelector2_cxx
